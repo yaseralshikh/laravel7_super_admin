864e72908831c17f52f67a1b7147f07e5f64e4e1
Yaser Alshikh <yaseralshikh@gmail.com>
2020-05-28 12:03:14 +0300

    fix Delete button function at layouts/dashboard/app.blade

--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -266,58 +266,58 @@
 <script>
     $(document).ready(function () {
 
-        $('.sidebar-menu').tree();
+    $('.sidebar-menu').tree();
 
-        //icheck
-        $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
-            checkboxClass: 'icheckbox_minimal-blue',
-            radioClass: 'iradio_minimal-blue'
+    //icheck
+    $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+        checkboxClass: 'icheckbox_minimal-blue',
+        radioClass: 'iradio_minimal-blue'
+    });
+
+    //delete
+    $('.delete').click(function (e) {
+
+        var that = $(this)
+
+        e.preventDefault();
+
+        var n = new Noty({
+            text: "@lang('site.confirm_delete')",
+            type: "warning",
+            killer: true,
+            buttons: [
+                Noty.button("@lang('site.yes')", 'btn btn-success mr-2', function () {
+                    that.closest('form').submit();
+                }),
+
+                Noty.button("@lang('site.no')", 'btn btn-primary mr-2', function () {
+                    n.close();
+                })
+            ]
         });
 
-        //delete
-        $('.delete').click(function (e) {
+        n.show();
 
-            var that = $(this)
+    });//end of delete
 
-            e.preventDefault();
+    // // image preview
+    // $(".image").change(function () {
+    //
+    //     if (this.files && this.files[0]) {
+    //         var reader = new FileReader();
+    //
+    //         reader.onload = function (e) {
+    //             $('.image-preview').attr('src', e.target.result);
+    //         }
+    //
+    //         reader.readAsDataURL(this.files[0]);
+    //     }
+    //
+    // });
 
-            var n = new Noty({
-                text: "@lang('site.confirm_delete')",
-                type: "warning",
-                killer: true,
-                buttons: [
-                    Noty.button("@lang('site.yes')", 'btn btn-success mr-2', function () {
-                        that.closest('form').submit();
-                    }),
+    CKEDITOR.config.language =  "{{ app()->getLocale() }}";
 
-                    Noty.button("@lang('site.no')", 'btn btn-primary mr-2', function () {
-                        n.close();
-                    })
-                ]
-            });
-
-            n.show();
-
-        });//end of delete
-
-        // image preview
-        $(".image").change(function () {
-        
-            if (this.files && this.files[0]) {
-                var reader = new FileReader();
-        
-                reader.onload = function (e) {
-                    $('.image-preview').attr('src', e.target.result);
-                }
-        
-                reader.readAsDataURL(this.files[0]);
-            }
-        
-        // });
-
-        CKEDITOR.config.language =  "{{ app()->getLocale() }}";
-
-    });//end of ready
+});//end of ready
     
 </script>
 @stack('scripts')
