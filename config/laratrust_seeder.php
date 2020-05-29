be9eb4832e9218385b1ac1631abffeb87c2dd781
Yaser Alshikh <yaseralshikh@gmail.com>
2020-05-29 04:43:01 +0300

    finalising Client Model

--- config/laratrust_seeder.php
+++ config/laratrust_seeder.php
@@ -13,9 +13,10 @@ return [
 
     'roles_structure' => [
         'super_admin' => [
-            'users' => 'c,r,u,d',
             'categories' => 'c,r,u,d',
             'products' => 'c,r,u,d',
+            'clients' => 'c,r,u,d',
+            'users' => 'c,r,u,d',
         ],
         'admin' => [],
     ],
