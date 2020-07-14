048c76ad099d3d244a30eb0fa25677299bd84097
Yaser Alshikh <yaseralshikh@gmail.com>
2020-07-14 16:58:28 +0300

    update ContactClients.blade.php

--- resources/views/emails/ContactClients.blade.php
+++ resources/views/emails/ContactClients.blade.php
@@ -1,34 +1,22 @@
 @component('mail::message')
 # فاتورة
 
+@component('mail::panel')
+
 تمت عملية الشراء بنجاح .
+
+@endcomponent
+
+@component('mail::table')
+| @lang('site.product') | @lang('site.category') | @lang('site.quantity') | @lang('site.price') |
+| -- |:----:| -----:| --------:|
+@foreach($order->products as $product)
+| {{ $product->name }} | {{ $product->category->name }} | {{ $product->pivot->quantity }} | {{ $product->sale_price }} |
+@endforeach
+@endcomponent
 
 @component('mail::button', ['url' => 'http://moad.yaseralshikh.com/cart/order/' . $order->id . '/invoice' , 'color' => 'green'])
 طباعة الفاتورة
-@endcomponent
-
-
-@component('mail::panel')
-    <table  class="table table-hover table-bordered table-responsive-sm">
-        <thead class="bg-dark text-white text-center">
-            <tr>
-                <th>@lang('site.product')</th>
-                <th class="text-center">@lang('site.category')</th>
-                <th class="text-center">@lang('site.quantity')</th>
-                <th class="text-center">@lang('site.price')</th>
-            </tr>
-        </thead>
-        <tbody class="text-center">
-            @foreach ($order->products as $product)
-                <tr>
-                    <td>{{ $product->name }}</td>
-                    <td class="text-center">{{ $product->category->name }}</td>
-                    <td class="text-center">{{ $product->pivot->quantity }}</td>
-                    <td class="text-center">{{ $product->sale_price }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
 @endcomponent
 
 
